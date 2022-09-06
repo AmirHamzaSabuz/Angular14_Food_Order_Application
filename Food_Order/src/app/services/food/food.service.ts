f56c588e24b06d544b97f039709afc64d4ef26e3
Amir Hamza <hamza_amir@yahoo.com>
2022-09-06 14:47:55 +0600

    Home Component, Router Configuration & Outlet, Food service

---
+++ Food_Order/src/app/services/food/food.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll():string[]
+  {
+    return[
+      '/assets/food-1.jpg',
+      '/assets/food-2.webp',
+      '/assets/food-3.webp',
+      '/assets/food-4.jpg',
+      '/assets/food-5.jpg',
+      '/assets/food-6.jpg',
+      '/assets/food-7.jpg',
+      '/assets/food-8.jpg',
+      '/assets/food-9.jpg',
+      '/assets/food-10.jpg',
+      '/assets/food-11.jpg',
+    ]
+  }
+}
