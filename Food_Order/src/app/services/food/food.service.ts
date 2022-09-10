4f184abc50edc43f6a153d1d6e92feb1824339de
Amir Hamza <hamza_amir@yahoo.com>
2022-09-10 09:40:55 +0600

    star rating, time & price

--- Food_Order/src/app/services/food/food.service.ts
+++ Food_Order/src/app/services/food/food.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import {Foods} from 'src/app/shared/models/food'
 @Injectable({
   providedIn: 'root'
 })
@@ -7,17 +7,98 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():string[]
+  getAll():Foods[]
   {
     return[
-      '/assets/food-1.jpg',
-      '/assets/food-2.webp',
-      '/assets/food-3.webp',
-      '/assets/food-4.jpg',
-      '/assets/food-5.jpg',
-      '/assets/food-6.jpg',
-      '/assets/food-7.jpg',
-      '/assets/food-8.jpg',
+      {
+        id:1,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-1.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      },
+      {
+        id:2,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-2.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      },
+      {
+        id:3,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-3.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      },
+      {
+        id:4,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-4.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      },
+      {
+        id:5,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-5.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      },
+      {
+        id:6,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-6.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      },
+      {
+        id:7,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-7.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      },
+      {
+        id:8,
+        name: 'Burger',
+        cookTime: '10-20',
+        price: 10,
+        favorite: false,
+        origins: ['italy'],
+        star: 4.5,
+        imageUrl: '/assets/images/foods/food-8.jpg',
+        tags:['Fastfood', 'Burger', 'Lunch'],
+      }
+
 
     ]
   }
